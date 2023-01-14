a6709a09bf7cee9e1c8ea4f9007d2c47b5c1efd2
Ariq Mukul <ariqmukul@gmail.com>
2023-01-14 13:31:51 -0500

    Adding toDo elements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import ToDoList from './ToDoList';
 
 function App() {
-  return (
+    const [todos, setTodos] = useState([])
+    const toDoNameRef = useRef()
+
+    function handleAddToDo(e) {
+        const name = toDoNameRef.current.value
+        if (name === '') return
+        console.log(name)
+    }
+
+    return (
     <>
-      <ToDoList />
-      <input type="text" />
-      <button> Add ToDos</button>
-      <button>Clear Completed ToDos</button>
-      <div>0 left to do </div>
+        <ToDoList todos={todos} />
+        <input ref={toDoNameRef} type="text" />
+        <button onClick={handleAddToDo}> Add ToDos</button>
+        <button>Clear Completed ToDos</button>
+        <div>0 left to do </div>
     </>
-  )
+    )
 }
 
 export default App;
